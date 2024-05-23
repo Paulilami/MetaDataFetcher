676bebc8255ff6980775e3f3f6dc577a88ce1545
Paul Ilami <108613093+Paulilami@users.noreply.github.com>
2024-05-24 01:33:49 +0200

    Update offc.js

--- offc.js
+++ offc.js
@@ -1,46 +1,55 @@
 const { Requester, Validator } = require('@chainlink/external-adapter');
+const crypto = require('crypto');
+const axios = require('axios');
 
-const createRequest = (input, callback) => {
-  // Validate input
+const createRequest = async (input, callback) => {
   const url = input.data.get;
+  const queryKey = input.data.queryKey || "";
+
   if (!url) {
     return callback(400, { error: 'URL not provided' });
   }
 
-  // Define request options
-  const options = {
-    method: 'GET',
-    url: url,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    const flattenedData = flattenJSON(data);
 
-  // Perform the request
-  Requester.request(options, (error, response, body) => {
-    if (error) {
-      return callback(500, Requester.errored(input.id, error));
-    } 
+    const keys = Object.keys(flattenedData);
+    const values = Object.values(flattenedData).map(value => {
+      if (typeof value === 'object' || isEncrypted(value)) {
+        return hashData(JSON.stringify(value));
+      }
+      return value;
+    });
 
-    try {
-      const data = JSON.parse(body);
-      const flattened = flattenJSON(data);
-      
-      callback(200, {
-        jobRunID: input.id,
-        data: {
-          keys: Object.keys(flattened),
-          values: Object.values(flattened)
-        },
-        result: flattened
+    if (queryKey) {
+      const filteredData = filterDataByKey(flattenedData, queryKey);
+      const filteredKeys = Object.keys(filteredData);
+      const filteredValues = Object.values(filteredData).map(value => {
+        if (typeof value === 'object' || isEncrypted(value)) {
+          return hashData(JSON.stringify(value));
+        }
+        return value;
       });
-    } catch (parseError) {
-      callback(500, { error: 'Failed to parse JSON', details: parseError });
+
+      return callback(200, {
+        jobRunID: input.id,
+        data: { keys: filteredKeys, values: filteredValues },
+        result: filteredData
+      });
     }
-  });
+
+    callback(200, {
+      jobRunID: input.id,
+      data: { keys, values },
+      result: flattenedData
+    });
+  } catch (error) {
+    callback(500, Requester.errored(input.id, error));
+  }
 };
 
-// Helper function to flatten JSON objects
 const flattenJSON = (data) => {
   const result = {};
 
@@ -67,8 +76,28 @@ const flattenJSON = (data) => {
   return result;
 };
 
-// Export the function for use by the Chainlink node
-module.exports.createRequest = createRequest;
+const isEncrypted = (value) => {
+  //...current placeholder -Paul
+  return false;
+};
 
-// Exports for testing
+const hashData = (data) => {
+  return crypto.createHash('sha256').update(data).digest('hex');
+};
+
+const filterDataByKey = (data, queryKey) => {
+  const result = {};
+  for (const key in data) {
+    if (key.includes(queryKey)) {
+      result[key] = data[key];
+    }
+  }
+  return result;
+};
+
+module.exports.createRequest = createRequest;
 module.exports.flattenJSON = flattenJSON;
+module.exports.hashData = hashData;
+module.exports.filterDataByKey = filterDataByKey;
+module.exports.isEncrypted = isEncrypted;
+
